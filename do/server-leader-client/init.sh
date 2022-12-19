761402ff34a99795483a556e9ebceadf9a6592b7
hector-medina-nubosas <hector.medina@nubosas.com>
2022-12-19 09:14:13 +0100

    add ansible playbook

--- do/server-leader-client/init.sh
+++ do/server-leader-client/init.sh
@@ -6,3 +6,5 @@ curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
 # Install ansible with pip.
 python3 -m pip install ansible
+# Run ansible playbook to install and configure nomad and dependencies.
+ansible-playbook https://github.com/hector-medina-nubosas/ansible2/blob/main/nomad/nomad.yml
