9333fcfc40d25b599cfa6093b3db53d8e45eeff3
hector-medina-nubosas <hector.medina@nubosas.com>
2022-12-20 12:59:03 +0100

    add git before ansible, otherwise ansible cannot run playbook

--- do/server-leader-client/init.sh
+++ do/server-leader-client/init.sh
@@ -6,5 +6,7 @@ curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
 # Install ansible with pip.
 python3 -m pip install ansible
+# Install git
+apt install git -y
 # Run ansible playbook to install and configure nomad and dependencies.
 ansible-pull -U https://github.com/hector-medina-nubosas/ansible2.git nomad/nomad.yml
