d74ce6198e583137e72b3ddd98f158628f52ca8e
hector-medina-nubosas <hector.medina@nubosas.com>
2022-12-27 17:19:33 +0100

    output private ip, useful to connect cluster in VPC

--- do/server-leader-client/outputs.tf
+++ do/server-leader-client/outputs.tf
@@ -5,3 +5,7 @@ output "urn" {
 output "ip" {
   value = digitalocean_droplet.server_leader_client.ipv4_address
 }
+
+output "ip_private" {
+  value = digitalocean_droplet.server_leader_client.ipv4_address_private
+}
