3386689dc8f3cbd8956d32777d3aaccbd9faad3d
hector-medina-nubosas <hector.medina@nubosas.com>
2022-12-29 15:32:26 +0100

    create local for treating server_join null values

--- do/server-leader-client/main.tf
+++ do/server-leader-client/main.tf
@@ -11,6 +11,10 @@ provider "digitalocean" {
     token = "${var.do_token}"
 }
 
+locals {
+  server_join = "${var.server_join == null ? null : jsonencode({server_join = {value = var.server_join}})}"
+}
+
 resource "digitalocean_droplet" "server_leader_client" {
     image = var.image
     name = var.name
@@ -19,8 +23,7 @@ resource "digitalocean_droplet" "server_leader_client" {
     ssh_keys = var.ssh_keys_fingerprints
     user_data = "${templatefile("${path.module}/init.sh", {
       server_role = var.server_role,
-      # server_join = "[ ${join(", ", [for ip in var.server_join : format("%q", ip)])} ]" 
-      server_join = jsonencode({server_join = {value = var.server_join}})
+      server_join = locals.server_join
     })}"
     vpc_uuid = var.vpc_id
 }
